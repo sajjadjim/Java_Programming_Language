d4995106228c83ff578f458b2e3afaf04f44a65f
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-11-21 21:48:50 +0600

    Delete Object Oriented Basic Java Project/src directory

--- Object Oriented Basic Java Project/src/Animal.java
+++
@@ -1,9 +0,0 @@
-public abstract class Animal {
-
-    String kingdom;
-
-    public Animal(String kingdom) {
-        this.kingdom = kingdom;
-    }
-    public abstract void display();
-}
--- Object Oriented Basic Java Project/src/Habits.java
+++
@@ -1,4 +0,0 @@
-public interface Habits {
-    public abstract void running();
-    public abstract void eating();
-}
